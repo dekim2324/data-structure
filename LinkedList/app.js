b41ecae92bc757df8a44ee87c2f6d323571dba8f
Dennis Kim <dekim2324@gmail.com>
2019-12-10 12:52:41 -0600

    linkedlist start

---
+++ LinkedList/app.js
@@ -0,0 +1,35 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    };
+
+    push(val) {
+        let newNode = new Node(val);
+        if(this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+    }
+    pop() {
+
+    }
+
+    shift() {
+
+    }
+    unshift(val) {
+
+    }
+}
